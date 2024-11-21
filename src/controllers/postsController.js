8dd836a66199dc5959ec9a5cae12336cfe5eacdd
Raquel <sxemillene@gmail.com>
2024-11-21 17:58:42 -0300

    Upload de imagens

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,6 +1,36 @@
-import { getTodosPosts } from "../models/postsModels.js";
+import fs from "fs"
+import { getTodosPosts, criarPost } from "../models/postsModels.js";
 
 export async function listarPosts(req, res) { //async e await são um par
     const posts = await getTodosPosts();
     res.status(200).json(posts); //200 significa sucesso
+}
+
+export async function postarNovoPost(req, res) {
+    const novoPost = req.body;
+    try{
+        const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado);
+    }catch (erroCriarPost){
+        console.error(error.message);
+        res.status(500).json({"Erro":"Falha na requisição"});
+    }
+}
+
+export async function uploadImagem(req, res) {
+    const novoPost = {
+        descricao: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    };
+
+    try {
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`
+        fs.renameSync(req.file.path, imagemAtualizada) //renomear o arquivo com o id
+        res.status(200).json(postCriado);  
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"})
+    }
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,24 @@
 import express from "express"
-import { listarPosts } from "../controllers/postsController.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+//Código de configuração do multer no windows
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ storage: storage }); //necessário criar a pasta uploads
 
 const routes = ((app) => {
     app.use(express.json()); //para o express transformar em jason
-    //Rota para buscar todos os posts
-    app.get("/posts", listarPosts);
+    app.get("/posts", listarPosts); //Rota para buscar todos os posts
+    app.post("/posts", postarNovoPost); //Rota para criar um novo post
+    app.post("/upload", upload.single("imagem"), uploadImagem);
 });
 
 export default routes;
